140448c4e8ad1a99432539bf9b3d96c8dc4214f8
DariusIII <dkrisan@gmail.com>
2019-12-17 13:42:59 +0100

    Remove stray square bracket.

--- TVMaze/TVShow.php
+++ TVMaze/TVShow.php
@@ -121,7 +121,7 @@ class TVShow extends TVProduction{
 		$this->weight = isset($show_data['weight']) ? $show_data['weight'] : null;
 		$this->network_array = isset($show_data['network']) ? $show_data['network'] : null;
 		$this->network = isset($show_data['network']['name']) ? $show_data['network']['name'] : null;
-		$this->webChannel = isset($show_data['webChannel']) ? $show_data['webChannel'] : null];
+		$this->webChannel = isset($show_data['webChannel']) ? $show_data['webChannel'] : null;
 		$this->country = isset($show_data['network']['country']['code']) ? $show_data['network']['country']['code'] : null;
 		if ($show_data['webChannel'] !== null && $show_data['webChannel']['country'] !== null) {
 			$this->country = $show_data['webChannel']['country']['code'];
